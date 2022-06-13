840a1dc9c13b1d2bc665034a0cd0c0f4f4bb1293
michkz <michkz@protonmail.com>
2022-06-13 19:36:26 +0200

    Change scope preparation

--- modules/prepareScope.py
+++ modules/prepareScope.py
@@ -1,4 +1,5 @@
-from modules.validateScope import validateScopeAddresses    ## Custom validation
+from modules.old_validateScope import validateScopeAddresses    ## Custom validation
+from modules.classes import Asset
 
 ## Function to prepare the scope for the different tools within the program
 def prepareScopeForTool(assets):
@@ -8,22 +9,40 @@ def prepareScopeForTool(assets):
 
     ## Save scope to new var scope_list
     for line in assets:
-        scope_list.append(line.strip("\n"))
+        scope_list.append(line)
+
 
     ## Assign the right assets to the right tools
     for tool in tool_list:
         current_tool = tool
         if current_tool == "host":
             for line in scope_list:
-                hostAssets = validateScopeAddresses(tool, line.strip("\n"),
+                main_asset = line.main_asset.strip("\n")
+                hostAssets = validateScopeAddresses(tool, main_asset,
                                                      hostAssets)
+                print("hostassets:{}\ncurrent line: {}\n".format(hostAssets, line))
         elif current_tool == "curl":
             for line in scope_list:
-                curlAssets = validateScopeAddresses(tool, line.strip("\n"),
+                line = line.main_asset.strip("\n")
+                curlAssets = validateScopeAddresses(tool, line,
                                                      curlAssets)
         elif current_tool == "nmap":
             for line in scope_list:
-                nmapAssets = validateScopeAddresses(tool, line.strip("\n"),
+                line = line.main_asset.strip("\n")
+                nmapAssets = validateScopeAddresses(tool, line,
                                                      nmapAssets)
 
     return hostAssets, curlAssets, nmapAssets
+
+def create_original_asset_list(original_scope):
+    # add og scope to main_asset property from class
+    # figure out how to add validated to same asset
+    # validate the main_asset and change to liking 
+    scope_list_of_objects = []
+    for asset in original_scope:
+        asset.strip("\n")
+        a = Asset(asset)
+        a.add_customer_contact("Bedrijf X","+31 6 13243546")
+        scope_list_of_objects.append(a)
+    # return list of objects
+    return scope_list_of_objects
