279715da562aa452adeceaca9dad5c22b5bfe3c5
michkz <michkz@protonmail.com>
2022-06-15 13:14:18 +0200

    Change scope preparation method

--- modules/prepareScope.py
+++ modules/prepareScope.py
@@ -1,38 +1,5 @@
-from modules.old_validateScope import validateScopeAddresses    ## Custom validation
+
 from modules.classes import Asset
-
-## Function to prepare the scope for the different tools within the program
-def prepareScopeForTool(assets):
-    tool_list = ["host", "curl", "nmap"]           ## List of tools in program
-    scope_list = []                                ## Creation of var scope_list
-    hostAssets, curlAssets, nmapAssets = [],[],[]  ## Creation of scope per tool
-
-    ## Save scope to new var scope_list
-    for line in assets:
-        scope_list.append(line)
-
-
-    ## Assign the right assets to the right tools
-    for tool in tool_list:
-        current_tool = tool
-        if current_tool == "host":
-            for line in scope_list:
-                main_asset = line.main_asset.strip("\n")
-                hostAssets = validateScopeAddresses(tool, main_asset,
-                                                     hostAssets)
-                print("hostassets:{}\ncurrent line: {}\n".format(hostAssets, line))
-        elif current_tool == "curl":
-            for line in scope_list:
-                line = line.main_asset.strip("\n")
-                curlAssets = validateScopeAddresses(tool, line,
-                                                     curlAssets)
-        elif current_tool == "nmap":
-            for line in scope_list:
-                line = line.main_asset.strip("\n")
-                nmapAssets = validateScopeAddresses(tool, line,
-                                                     nmapAssets)
-
-    return hostAssets, curlAssets, nmapAssets
 
 def create_original_asset_list(original_scope):
     # add og scope to main_asset property from class
