9bafc3fc4fefaeb4fa447de2b1591ae6c48f1991
michkz <michkz@protonmail.com>
2022-06-15 13:13:01 +0200

    Add main functionality to the program

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+from commands.host import hostTool                        # Command to run host
+from commands.curl import curlTool                        # Command to run curl
+from commands.nmap import nmapTool                        # Command to run nmap
+from modules.classes import Template                        # Template class
+from modules.prepareScope import create_original_asset_list # Preparation script
+from modules.validateScope import validateScopeObjects      # Validation script
+import argparse                                             # Argparse library
 
-from commands.host import hostCommand                 ## command to run host
-from commands.curl import curlCommand                 ## command to run curl
-from commands.nmap import nmapCommand                 ## command to run nmap
-from modules.prepareScope import prepareScopeForTool  ## script to prepare scope
-from modules.classes import classes                   ## Load in the classes
 
-import argparse                                       ## argparse library
-
-
-logo ="""
-     ▌ ▐·      ▪  ·▄▄▄▄  .▄▄ ·  ▄▄·  ▄▄▄·  ▐ ▄ 
+logo = """
+     ▌ ▐·      ▪  ·▄▄▄▄  .▄▄ ·  ▄▄·  ▄▄▄·  ▐ ▄
     ▪█·█▌▪     ██ ██▪ ██ ▐█ ▀. ▐█ ▌▪▐█ ▀█ •█▌▐█
     ▐█▐█• ▄█▀▄ ▐█·▐█· ▐█▌▄▀▀▀█▄██ ▄▄▄█▀▀█ ▐█▐▐▌
      ███ ▐█▌.▐▌▐█▌██. ██ ▐█▄▪▐█▐███▌▐█ ▪▐▌██▐█▌
@@ -18,47 +17,79 @@ logo ="""
 """
 
 def main():
-     #// - prepare scope
-     #// - run tools: nmap, curl, host
-     #TODO gather results
-     #TODO sendToTemplate
-     print(logo)
 
-     ## Argparse code that reads the input file
-     parser = argparse.ArgumentParser(
-                         description="""
-                         This is a reconnaissance program that gathers useful 
+    print(logo)
+    #TODO add arg to turn on safe mode so nmap doesnt scan hosts
+    # Argparse code that reads the input file
+    parser = argparse.ArgumentParser(
+                        description="""
+                         This is a reconnaissance program that gathers useful
                          information about the given scope
                          """)
-                         
-     parser.add_argument('-f', '--filename',
-                         help="""Provide the program with a scope. f.e. 
-                         main.py -f [file.txt]""", 
+
+    parser.add_argument('-f', '--filename',
+                         help="""Provide the program with a scope. f.e.
+                         main.py -f [file.txt]""",
                          type=argparse.FileType('r'))
-                         
-     args = parser.parse_args()
+    args = parser.parse_args()
+    complete_scope = []
+    complete_scope = create_original_asset_list(args.filename)
+    print("[*] Validating scope...")
+    try:
+        validated_scope = []
+        # Validate the given scope
+        validated_scope = validateScopeObjects(complete_scope)
+    except Exception as e:
+        print("[!] Something went wrong during the validation, aborting. {}" \
+                                                                    .format(e))
+    print("[*] Scope successfully validated.")
+    for asset in validated_scope:
+        if asset.host_uri:
+            try:
+                # Run the host_uri of the current main_asset through Host
+                print("[*] Executing host tool for: {}".format(asset.host_uri))
+                hostTool(asset)
+            except Exception as e:
+                print("[!] Something went wrong during the host scan.")
+            print("[*] Host tool completed")
+        if asset.curl_uri:
+            try:
+                # Run the curl_uri of the current main_asset through Curl
+                print("[*] Executing curl tool for: {}".format(asset.curl_uri))
+                curlTool(asset)
+            except Exception as e:
+                print("[!] Something went wrong during the curl scan.")
+            print("[*] Curl tool completed")
+        if asset.nmap_uri:
+            try:
+                print("[*] Executing nmap tool for: {}".format(asset.nmap_uri))
+                nmapTool(asset)
+            except Exception as e:
+                print("[!] Something went wrong during the nmap scan.")
+            print("[*] Nmap tool completed")
 
-     hostAssets, curlAssets, nmapAssets = prepareScopeForTool(args.filename)
+    # Printing out the results
+    # print("\n\n-=- The results are in -=-")
+    # for asset in validated_scope:
+    #     print("\nMain asset: {}".format(asset.main_asset.strip("\n")))
+    #     if asset.host_uri:
+    #         print("|= Host output: {}".format(asset.host_results))
+    #         if asset.host_zonetransfer_results:
+    #             # print("|= Zonetransfer output: {}"\
+    #                 # .format(asset.host_zonetransfer_results))
+    #             pass
+    #     if asset.curl_uri:
+    #         print("|= Curl output: {}".format(asset.curl_results))
 
+    #     if asset.nmap_uri:
+    #         print("|= Nmap output: {}\n".format(asset.nmap_results))
 
-     ## Sending file contents to host command
-     print("-=- Commencing host command -=-")
-     hostCommand(hostAssets)
-     print("-=- Now exiting host command -=-")
-
-     ## Sending file contents to cURL command
-     print("-=- Commencing cURL command -=-")
-     curlCommand(curlAssets)
-     print("-=- Now exiting cURL command -=-")
-
-     #? - do not uncomment unless on own network due scanning
-     # #* Sending file contents to nmap command
-     print("-=- Commencing nmap command -=-")
-     nmapCommand(nmapAssets)
-     print("-=- Now exiting nmap command -=-")
-
-     print("Job's done")
+    
+    print("[*] Creating report...")
+    template_out = Template('PentestReport.jinja').render(validated_scope)
+    with open('PentestReport.md','w') as output:
+        output.write(template_out)
+    print("[*] Report created successfully")
 
 if __name__ == '__main__':
-     main()
-
+    main()
