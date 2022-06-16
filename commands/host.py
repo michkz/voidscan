18bec9e0d651ea2fb677aadd8a524a2746758a93
michkz <michkz@protonmail.com>
2022-06-16 22:40:21 +0200

    Validate through pep8 guidelines

--- commands/host.py
+++ commands/host.py
@@ -1,134 +1,128 @@
 import subprocess
-from xml.etree.ElementInclude import include				# Subprocess library
-from modules.classes import Asset							# Import Asset class
+
+
 def hostTool(asset):
-	host_dict = {}
-	found_ipv4 = []
-	found_mail = []
-	found_ipv6 = []
-	found_nameserver = []
-	zonetransfer_results = []
-	zonetransfer_dict = {}
-	debug_list = []
-	error_count = 0
-	uri = ''
-	ttl = ''
-	inc = ''
-	axfr = '' 
-	result = ''
+    host_dict = {}
+    found_ipv4 = []
+    found_mail = []
+    found_ipv6 = []
+    found_nameserver = []
+    zonetransfer_results = []
+    zonetransfer_dict = {}
+    debug_list = []
+    uri = ''
+    ttl = ''
+    inc = ''
+    axfr = ''
+    result = ''
 
-	try:
-		try:
-			# Run the host command through subprocess
-			host_cmd = subprocess.Popen(('host {}'.format(asset.host_uri)),shell=True,
-										stdout=subprocess.PIPE)
-			# loop through each address of the scope
-			for line in host_cmd.stdout:
-				line = line.decode('utf-8').strip("\n")
-				# Grab IP from line, store in var ip
-				if "has address" in line:
-					*_, ip = line.split(' ')
-					found_ipv4.append(ip)
-				# Grab mail from line, store in var mail
-				elif "mail is" in line:
-					*_, mail = line.split(' ')
-					found_mail.append(mail)
-				# Grab IPv6 from line, store in var ipv6
-				elif "IPv6" in line:
-					*_, ipv6 = line.split(' ')
-					found_ipv6.append(ipv6)
-				
-				host_dict = {"IPv4": found_ipv4,"Mail":found_mail,"IPv6":found_ipv6}
-		except TypeError as e:
-			print(e)
-		except Exception as e:
-			print("This went wrong: {}".format(e))
+    try:
+        try:
+            # Run the host command through subprocess
+            host_cmd = subprocess.Popen(('host {}'.format(asset.host_uri)),
+                                        shell=True, stdout=subprocess.PIPE)
+            # Loop through the results
+            for line in host_cmd.stdout:
+                line = line.decode('utf-8').strip("\n")
+                # Grab IP from line, store in var ip
+                if "has address" in line:
+                    *_, ip = line.split(' ')
+                    found_ipv4.append(ip)
+                # Grab mail from line, store in var mail
+                elif "mail is" in line:
+                    *_, mail = line.split(' ')
+                    found_mail.append(mail)
+                # Grab IPv6 from line, store in var ipv6
+                elif "IPv6" in line:
+                    *_, ipv6 = line.split(' ')
+                    found_ipv6.append(ipv6)
+        except TypeError as e:
+            print("[!] The following TypeError occurred: {}".format(e))
+        except Exception as e:
+            print("[!] The following went wrong: {}".format(e))
+        try:
+            # Run host command for nameserver information
+            host_cmd = subprocess.Popen(('host -t ns {}'
+                                        .format(asset.host_uri)), shell=True,
+                                        stdout=subprocess.PIPE)
+            for line in host_cmd.stdout:
+                line = line.decode('utf-8').strip("\n")
+                # Grab name server from line, store in var nameserver
+                if "name server" in line:
+                    *_, nameserver = line.split(" ")
+                    found_nameserver.append(nameserver)
+        except TypeError as e:
+            print("[!] The following TypeError occurred: {}".format(e))
+        except Exception as e:
+            print("[!] The following went wrong: {}".format(e))
+        # Add results to the host dictionary
+        host_dict = {"IPv4": found_ipv4, "Mail": found_mail,
+                     "IPv6": found_ipv6, "NameServer": found_nameserver}
+        # Add host dictionary to the asset
+        asset.add_host_findings(host_dict)
 
-		try:
-			# Run host command for nameserver information
-			host_cmd = subprocess.Popen(('host -t ns {}'
-										.format(asset.host_uri)),shell=True,
-										stdout=subprocess.PIPE)
-			for line in host_cmd.stdout:
-				line = line.decode('utf-8').strip("\n")
-				# Grab name server from line, store in var nameserver
-				if "name server" in line:
-					*_, nameserver = line.split(" ")
-					found_nameserver.append(nameserver)
-				
-		except TypeError as e:
-			print(e)
-		except Exception as e:
-			print("This went wrong: {}".format(e))
-		
-		host_dict = {"IPv4": found_ipv4,"Mail":found_mail,"IPv6":found_ipv6,"NameServer": found_nameserver}
-		asset.add_host_findings(host_dict)
+        # This is where the DNS Zonetransfer happens
+        try:
+            # Boolean to start grabbing useful dns info from host cmd
+            gatherIntel = False
+            for ns in found_nameserver:
+                # Run host command for dns zone transfer information
+                host_cmd = subprocess.Popen(('host -t axfr {} {}'.format
+                                            (asset.host_uri, ns)), shell=True,
+                                            stdout=subprocess.PIPE)
+                for line in host_cmd.stdout:
+                    line = line.decode('utf-8').strip("\n")
+                    if "ANSWER SECTION" in line:
+                        # Start capturing results for data dict
+                        gatherIntel = True
+                        continue
+                    if gatherIntel:
+                        zonetransfer_results = line.rsplit("\t", 1)
+                        # Add lines to the debug_list
+                        debug_list.append(zonetransfer_results)
+                        # Stop capturing results for data dict
+                        if len(line) < 2:
+                            gatherIntel = False
+        except TypeError as e:
+            print("[!] The following TypeError occurred: {}".format(e))
+        except Exception as e:
+            print("[!] The following went wrong: {}".format(e))
+        # Save the DNS Zonetransfer answers to split up variables
+        for line in debug_list:
+            try:
+                # If line is not empty, move on to process through split
+                if len(line[0]) >= 2 and len(line[1]) >= 2:
+                    if "\t" in line[0]:
+                        try:
+                            # Split line[0] into 4 on \t
+                            uri, ttl, inc, axfr = line[0].split("\t")
+                        except Exception as e:
+                            # Throw an exception, but continue
+                            pass
+                        try:
+                            # Split line[0] first into 2 parts then split on \t
+                            first, second = line[0].split(" ")
+                            uri = first
+                            ttl, inc, axfr = second.split("\t")
+                        except Exception as e:
+                            # Throw an exception, but continue
+                            pass
+                    # If line has no \t, split on space once to process
+                    elif "\t" not in line[0]:
+                        first, second = line[0].split(" ", 1)
+                        uri = first
+                        ttl, inc, axfr = second.split(" ")
+                    result = line[1]
+                    # Add results to the zonetransfer dictionary
+                    zonetransfer_dict = {"URI": uri, "TTL": ttl, "IN": inc,
+                                         "AXFR": axfr, "Result": result}
+                    # Add zonetransfer dictionary to the asset results
+                    asset.add_host_zonetransfer_results(zonetransfer_dict)
 
-		# This is where the DNS Zonetransfer happens
-		try:
-			# Boolean to start grabbing useful dns info from host cmd
-			gatherIntel = False
-			for ns in found_nameserver:
-				# Run host command for dns zone transfer information
-				host_cmd = subprocess.Popen(('host -t axfr {} {}'.format
-											(asset.host_uri,ns)),shell=True,
-											stdout=subprocess.PIPE)
-				for line in host_cmd.stdout:
-					line = line.decode('utf-8').strip("\n")
-					if "ANSWER SECTION" in line:
-						# Start capturing results for data dict
-						gatherIntel = True
-						continue
-					if gatherIntel:
-						zonetransfer_results = line.rsplit("\t", 1)
-						debug_list.append(zonetransfer_results)
-						# Stop capturing results for data dict
-						if len(line) < 2:
-							gatherIntel = False
-		except TypeError as e:
-			print(e)
-		except Exception as e:
-			print("This went wrong: {}".format(e))
-
-		#  The list with fetched results from the DNS Zonestransfer ANSWERS 
-		#  section
-		for line in debug_list:
-			try: 
-				# If line is not empty, move on to process through split
-				if len(line[0]) >= 2 and len(line[1]) >= 2:
-					if "\t" in line[0]:
-						try:
-							# Split line[0] into 4 on \t
-							uri, ttl, inc, axfr = line[0].split("\t")
-						except Exception as e:
-							# Add one error if it goes wrong to print out at the
-							# end.
-							error_count += 0
-						try:
-							# Split line[0] first into 2 parts then split on \t
-							first, second = line[0].split(" ")
-							uri = first
-							ttl, inc, axfr = second.split("\t")
-						except Exception as e:
-							# Add one error if it goes wrong to print out at the
-							# end.
-							error_count += 0
-					# If line has no \t, split on space once to process
-					elif "\t" not in line[0]:
-						first, second = line[0].split(" ", 1)
-						uri = first
-						ttl, inc, axfr = second.split(" ")
-					result = line[1]
-					
-					zonetransfer_dict = {"URI": uri, "TTL":ttl,"IN":inc,"AXFR":axfr,"Result":result}
-					asset.add_host_zonetransfer_results(zonetransfer_dict)
-
-			except Exception as e: 
-				# Throw an exception when the splitting goes wrong.
-				pass
-		
-	except TypeError as e:
-		print("Something went wrong while running the scan, please refer to: {}"
-			.format(e))
-	except Exception as e:
-			print("This went wrong: {}".format(e))
+            except Exception as e:
+                # Throw an exception when the splitting goes wrong.
+                pass
+    except TypeError as e:
+        print("[!] The following TypeError occurred: {}".format(e))
+    except Exception as e:
+        print("[!] The following went wrong: {}".format(e))
--- commands/nmap.py
+++ commands/nmap.py
@@ -1,29 +1,24 @@
-import socket
 import subprocess
-import threading
-from modules.classes import Asset
 
 
 def nmapTool(asset):
-
-
     scan_results = {}
-    # Creation of the data dictionary to save results to.
-    
     try:
-        nmap_cmd = subprocess.Popen(('nmap {}'.format(asset.nmap_uri)), shell=True, stdout=subprocess.PIPE)
+        nmap_cmd = subprocess.Popen(('nmap {}'.format(asset.nmap_uri)),
+                                    shell=True, stdout=subprocess.PIPE)
         for line in nmap_cmd.stdout:
             line = line.decode('utf-8').strip("\n")
-            if "report for" in line:
-                *_, host = line.split(' ')
-
+            # Save the open port and service to variables
             if "open" in line:
                 org_line = line
                 ip_port, *_ = line.split(' ')
                 *_, ip_serv = org_line.split(' ')
-                scan_results = {"Port":ip_port.strip("\n"),"Service":ip_serv.strip("\n")}
+                # Save the results to scan_results
+                scan_results = {"Port": ip_port.strip("\n"),
+                                "Service": ip_serv.strip("\n")}
+                # Add the scan_results to the asset results
                 asset.add_nmap_findings(scan_results)
     except TypeError as e:
-        print(e)
+        print("[!] The following TypeError occurred: {}".format(e))
     except Exception as e:
-        print("This went wrong: {}".format(e))
+        print("[!] The following went wrong: {}".format(e))
